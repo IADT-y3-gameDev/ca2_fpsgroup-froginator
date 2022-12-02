cba8e564077eb18f6e6e220bad3f340c16e85b59
Kenji Dasal <35777115+KenjiDasal@users.noreply.github.com>
2022-12-02 11:31:54 +0000

    Moving To Main

---
+++ src/Assets/Scripts/SendDamageToPlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendDamageToPlayer : MonoBehaviour
+{
+    public int damage;
+
+    void OnCollisionStay(Collision other)
+    {
+        
+        if (other.transform.CompareTag("Player"))
+        {
+            
+            other.transform.SendMessage("ApplyDamage", damage);
+        }
+    }
+}

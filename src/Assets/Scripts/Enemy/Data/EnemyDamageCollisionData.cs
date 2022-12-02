0a33471ec7163b9f1c2ae4540f970cdf9442aaec
Kenji Dasal <35777115+KenjiDasal@users.noreply.github.com>
2022-12-02 12:10:06 +0000

    Test

---
+++ src/Assets/Scripts/Enemy/Data/EnemyDamageCollisionData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class EnemyDamageCollisionData : MonoBehaviour
+{
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.collider.transform.CompareTag("Player"))
+        {
+            other.transform.SendMessage("ApplyDamage", 1);
+        }
+    }
+}
